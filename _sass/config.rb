23703f02887f173691c42c8ca2511163a93a0dd3
Tim Proffitt <tim.proffitt@gmail.com>
2014-06-14 16:28:51 -0500

    restructure assets folder

--- _sass/config.rb
+++ _sass/config.rb
@@ -3,9 +3,9 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 sass_dir = "./"
-css_dir = "../css"
-images_dir = "../img"
-javascripts_dir = "../js"
+css_dir = "../assets/css"
+images_dir = "../assets/images"
+javascripts_dir = "../assets/js"
 relative_assets = true
 
 # http_path = "/"
